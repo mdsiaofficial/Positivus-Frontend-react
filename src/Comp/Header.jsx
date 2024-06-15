c09af626121d6bc67be35130f22bc3b5f6f097cb
mdsiaofficial <mdsiaofficial@gmail.com>
2024-06-15 17:54:50 +0600

    "Updated Header and Navbar components, added state to Navbar for menu toggle, and modified CSS to include transition class."

--- src/Comp/Header.jsx
+++ src/Comp/Header.jsx
@@ -4,14 +4,15 @@ const Header = () => {
   return (
     <div>
 
-      <div className="flex flex-col md:flex-row mx-10 my-3">
-        <nav className='flex flex-col md:flex-row'>
-          <h1 className="text-3xl order-1 font-bold ">Navigating the digital landscape for success!</h1>
-          <p className="text-lg order-3 md:order-2  ">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
-          <button className='bg-pBlack text-pWhite rounded-lg w-fit p-3 mt-3 order-4 md:order-3'>Book a consultation</button>
-          <img src={header1} alt="" className='order-2 md:hidden md:w-[600px] md:h-[515px]'/>
+      <div className="flex flex-col md:flex-row mx-10 my-3 md:justify-center md:items-center">
+        <nav className='flex flex-col md:gap-5'>
+          <h1 className="text-4xl md:text-6xl order-1 font-bold ">Navigating the digital landscape for success!</h1>
+          <p className="text-lg order-3 md:order-2 ">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
+          <button className='bg-pBlack text-pWhite rounded-lg w-fit p-3 mt-5 order-4 md:order-3'>Book a consultation</button>
+
+          <img src={header1} alt="" className='order-2 md:hidden p-3 md:w-[600px] md:h-[515px]'/>
         </nav>
-        <img src={header1} alt="" className='hidden md:block md:w-[600px] md:h-[515px]'/>
+        <img src={header1} alt="" className='hidden md:block p-3 md:w-[600px] md:h-[515px]'/>
       </div>
     </div>
   )
--- src/Global/Navbar.jsx
+++ src/Global/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { logo1 } from './Imgs'
 import { Link } from 'react-router-dom'
 import { BsMenuButton } from 'react-icons/bs'
-
+import { CgClose } from 'react-icons/cg'
 const Navbar = () => {
+
+  const [menu, setMenu] = useState(false);
+  
   return (
     <div className=''>
 
@@ -24,23 +27,29 @@ const Navbar = () => {
       </section>
 
       {/* nav menu mobile */}
-      <section className='flex flex-row md:hidden items-center justify-around my-4'>
+      <section className='flex flex-row md:hidden items-center justify-around my-4 transition-all'>
         {/* logo */}
         <section className=''>
           <img src={logo1} alt="" />
         </section>
         {/* menu */}
         <section>
-          <BsMenuButton className='text-4xl' />
-          {/* <ul className='flex absolute flex-col gap-[40px] text-center bg-slate-600 rounded-2xl text-white right-0 top-16 p-7'>
-            <li>About Us</li>
-            <li>Services</li>
-            <li>Use Cases</li>
-            <li>Pricing</li>
-            <li>Blog</li>
-            <li className='border-2 rounded-lg border-black px-[35px] py-[20px]'>Request a Quote</li>
-          </ul> */}
-
+          <button onClick={() => setMenu(!menu)}>
+            {menu ? <CgClose className='text-4xl' /> : <BsMenuButton className='text-4xl' />}
+          </button>
+          {menu && (
+            <ul className='flex absolute flex-col gap-[40px] text-center bg-slate-600 rounded-2xl text-white right-0 top-0 p-4'>
+              <button onClick={() => setMenu(!menu)}>
+                <CgClose className='text-4xl' />
+              </button>
+              <li>About Us</li>
+              <li>Services</li>
+              <li>Use Cases</li>
+              <li>Pricing</li>
+              <li>Blog</li>
+              <li className='border-2 rounded-lg border-black px-[35px] py-[20px]'>Request a Quote</li>
+            </ul>
+          )}
         </section>
       </section>
 
