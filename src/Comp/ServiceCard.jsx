532b1630e6d3a1430c8afd6e6bd8fbf390f587eb
mdsiaofficial <mdsiaofficial@gmail.com>
2024-06-16 00:41:01 +0600

    "Updated ServiceCard component: added width classes to icon and text, and modified text class to include font size."

--- src/Comp/ServiceCard.jsx
+++ src/Comp/ServiceCard.jsx
@@ -8,8 +8,8 @@ const ServiceCard = ({title, img, icon, bg}) => {
         <div className='flex flex-col justify-around'>
           <h1 className={`bg-primary w-fit p-1 rounded-lg`}>{title}</h1>
           <nav className='flex gap-2 items-center'>
-            <img src={icon} alt="" className='' />
-            <p className={`text-${bg==='pWhite' || bg==='primary'? 'black' : 'pWhite'  }`}>Learn More</p>
+            <img src={icon} alt="" className='w-6 md:w-auto' />
+            <p className={`text-${bg==='pWhite' || bg==='primary'? 'black' : 'pWhite'  } text-sm md:text-lg`}>Learn More</p>
           </nav>
         </div>
         <img src={img} alt="" className='w-[170px]  md:w-[210px]'/>
